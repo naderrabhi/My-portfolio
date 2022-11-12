48709c8a126fa3b107d8bc247421077415ad690f
nader rabhi <naderrabhi93@outlook.com>
2022-11-12 10:13:56 +0100

    update

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./footer.css";
 const Footer = () => {
   return (
     <div className="app___footer">
-        <a href="dev.nader@gmail.com">dev.nader@gmail.com</a>
+        <a href="dev.nader@gmail.com">dvp.nader@gmail.com</a>
         <p>+216 97 076 800</p>
         <p className="app__footer-copy">COPYRIGHT©2022 <span>NDRDEV</span> ALL RIGHTS RESERVED</p>
     </div>
